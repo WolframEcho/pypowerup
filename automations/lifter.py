266385e655ae49a7054946f926ea39553be108e4
Julian Baeza <julian.baeza@thedropbears.org.au>
2018-01-24 17:49:03 +1100

    Updating with Matthew.

--- automations/lifter.py
+++ automations/lifter.py
@@ -2,24 +2,48 @@ from magicbot import StateMachine, state, timed_state
 
 from components.lifter import Lifter
 
+from componets.intake import Intake
+
 
 class LifterAutomation(StateMachine):
     lifter: Lifter
+    intake: Intake
 
     @state(First=True, must_finish=True)
     def move(self):
         """Move to lifter height according to button press(5 buttons)"""
+    
 
     @state(must_finish=True)
     def move_complete(self):
-        """This state makes sure that you are at you set height"""
+        """This state makes sure that you are at you set height."""
         if self.lifter.get_pos() == self.setpos:
-            self.next_state("unload")
+            self.next_state("eject")
 
     @timed_state(duration=0.5, next_state="reset", must_finish=True)
-    def kick(self):
+    def eject(self):
+        """Ejects cube from mechanism as height is reached."""
         self.intake.intake_clamp(False)
         self.intake.intake_push(True)
 
     @state(must_finish=True)
     def reset(self):
+        self.intake.intake_push(False)
+
+
+
+
+
+
+
+
+
+
+                if self.switchButton():
+            self.next_state("move_complete")
+        if self.lower_scaleButton():
+            self.next_state("move_complete")
+        if self.balanced_scaleButton():
+            self.next_state("move_complete")
+        if self.upper_scaleButton():
+            self.next_state("move_complete")
